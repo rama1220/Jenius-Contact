fdb9d5a8622e9e876f8efe2b622a4350a79a40e6
Muchammad Ramadhan <muchammadramadhan12@gmail.com>
2024-04-05 10:18:25 +0700

    first commit

--- src/Component/FormContact.jsx
+++ src/Component/FormContact.jsx
@@ -1,5 +1,39 @@
 import Head from "./Head";
+import { useDispatch } from "react-redux";
+import { addData } from "../feature/dataSlice";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function FormContact() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [imageSrc, setImageSrc] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(null);
+
+  console.log(firstName, lastName, age, imageSrc);
+
+  const handleImageChange = (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+      setImageSrc(reader.result);
+    };
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("oke");
+
+    const newData = {
+      photo: imageSrc,
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+    };
+    dispatch(addData(newData));
+    navigate("/menu");
+  };
+
   return (
     <>
       <div className="container-box">
@@ -9,23 +43,25 @@ export default function FormContact() {
             <h1>Add Contact</h1>
             <div className="center-form">
               <div className="form-box">
-                <form action="">
+                <form>
                   <label htmlFor="foto">
-                    <input type="file" id="foto" />
+                    <input type="file" id="foto" onChange={handleImageChange} />
                   </label>
                   <label htmlFor="firstName">
-                    <input type="text" id="firstName" placeholder="First Name" />
+                    <input type="text" id="firstName" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} />
                   </label>
 
                   <label htmlFor="lastName">
-                    <input type="text" id="lastName" placeholder="Last Name" />
+                    <input type="text" id="lastName" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
                   </label>
                   <label htmlFor="age">
-                    <input type="number" id="age" placeholder="Age" />
+                    <input type="number" id="age" placeholder="Age" onChange={(e) => setAge(e.target.value)} />
                   </label>
                 </form>
 
-                <button>Save</button>
+                <button type="submit" onClick={handleSubmit}>
+                  Save
+                </button>
               </div>
             </div>
           </div>
